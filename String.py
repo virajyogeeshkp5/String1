658ac93c865d18525a42349a2bf9d556e7a5b6cc
YOGEESH K P <yogeesh793@gmail.com>
2024-11-30 01:24:12 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,21 @@
+#string
+print('Hello world')
+name='yogee'
+age=29
+
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+print(name[0: ])
+print(name[0: :2])
